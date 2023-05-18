2b81cfc4be331803a51bdc3bb0f6b73e0a1abe42
Kyle Scott <kyscott@umich.edu>
2023-05-18 16:26:54 -0400

    wip

--- hooks/useAllowance.ts
+++ hooks/useAllowance.ts
@@ -1,34 +1,79 @@
 import type { HookArg } from "./internal/types";
 import { getQueryKey } from "./internal/useQueryKey";
-import { userRefectchInterval } from "./internal/utils";
 import { useChainID } from "./useChain";
 import { Token } from "@/lib/currency";
-import { allowance } from "@/lib/reverseMirage/token";
-import { mergeQueryKeys } from "@lukemorales/query-key-factory";
-import { createQueryKeys } from "@lukemorales/query-key-factory";
-import { useQuery } from "@tanstack/react-query";
+import { allowance, balance } from "@/lib/reverseMirage/token";
+import { createQueryKeyStore } from "@lukemorales/query-key-factory";
+import { QueryKey, UseQueryOptions, useQuery } from "@tanstack/react-query";
+import { useMemo } from "react";
 import { objectKeys } from "ts-extras";
 import { Address, usePublicClient } from "wagmi";
 
-export const useAllowanceQuery = () => {
+export const useQueryFactory = () => {
   const publicClient = usePublicClient();
   const chainID = useChainID();
 
-  return createQueryKeys("allowance", {
+  const queries = { allowance } as const;
+
+  const rm = {
     allowance: (args: Partial<Parameters<typeof allowance>[1]>) => ({
       queryKey: getQueryKey(allowance, args, chainID),
-      queryFn: allowance(publicClient, args as Parameters<typeof allowance>[1]),
-      // if any of the args are undefined then this is invalid
+      queryFn: () =>
+        allowance(publicClient, args as Parameters<typeof allowance>[1]),
       enabled: objectKeys(args).some((key) => args[key] === undefined),
       staleTime: Infinity,
-      refetchInterval: userRefectchInterval,
+      context: undefined,
     }),
-  });
-};
+  } as const satisfies {
+    [query in keyof typeof queries]: (
+      args: Partial<Parameters<typeof queries[query]>[1]>,
+    ) => UseQueryOptions<
+      ReturnType<typeof queries[query]>,
+      Error,
+      ReturnType<typeof queries[query]>,
+      QueryKey
+    >;
+  };
 
-export const useQueries = () => {
-  const allowanceQuery = useAllowanceQuery();
-  return mergeQueryKeys(allowanceQuery);
+  // TODO: add query context for refetch interval
+  const reverseMirage = objectKeys(queries).reduce(
+    (acc, cur) => {
+      const read = queries[cur];
+
+      return {
+        ...acc,
+        [cur]: (args) =>
+          ({
+            queryKey: getQueryKey(read, args, chainID),
+            queryFn: () =>
+              read(publicClient, args as Parameters<typeof read>[1]),
+            enabled: objectKeys(args).some((key) => args[key] === undefined),
+            staleTime: Infinity,
+          }) as const,
+      } as const;
+    },
+    // {} as {
+    //   [query in keyof typeof queries]: NonNullable<
+    //     Parameters<typeof createQueryKeyStore>[0][string]
+    //   >[string];
+    // },
+    {} as {
+      [query in keyof typeof queries]: (
+        args: Partial<Parameters<typeof queries[query]>[1]>,
+      ) => Omit<
+        UseQueryOptions<ReturnType<typeof queries[query]>>,
+        "queryKey"
+      > & { queryKey: QueryKey };
+    },
+  );
+
+  return useMemo(
+    () =>
+      createQueryKeyStore({
+        reverseMirage,
+      }),
+    [chainID],
+  );
 };
 
 export const useAllowance = <T extends Token>(
@@ -36,7 +81,7 @@ export const useAllowance = <T extends Token>(
   address: HookArg<Address>,
   spender: HookArg<Address>,
 ) => {
-  const queries = useQueries();
+  const queries = useQueryFactory();
 
-  return useQuery(queries.allowance.allowance({ token, address, spender }));
+  return useQuery(queries.reverseMirage.allowance({ token, address, spender }));
 };
