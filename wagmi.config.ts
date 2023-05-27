65f92debe1edfd43c3148a52fd03760cd758e091
Kyle Scott <kyscott@umich.edu>
2023-05-27 12:28:32 -0400

    update actions for testing

--- wagmi.config.ts
+++ wagmi.config.ts
@@ -1,12 +1,12 @@
-import { defineConfig } from '@wagmi/cli'
-import { foundry } from '@wagmi/cli/plugins'
+import { defineConfig } from "@wagmi/cli";
+import { foundry } from "@wagmi/cli/plugins";
 
 export default defineConfig({
-  out: 'src/_test/generated.ts',
+  out: "src/_test/generated.ts",
   contracts: [],
   plugins: [
     foundry({
-      project: 'contracts/',
+      project: "contracts/",
     }),
   ],
-})
+});
