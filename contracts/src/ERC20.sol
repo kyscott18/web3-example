410e07029bfda5da4128f1f187031be10e68452f
Kyle Scott <kyscott@umich.edu>
2023-05-27 09:58:20 -0400

    start to add foundry

---
+++ contracts/src/ERC20.sol
@@ -0,0 +1,3 @@
+import { ERC20 as solmateERC20 } from "solmate/ERC20.sol";
+
+contract ERC20 is solmateERC20 {}
--- src/_test/setup.ts
+++ src/_test/setup.ts
@@ -1,6 +1,21 @@
-import { forkBlockNumber } from "./constants";
-import { setBlockNumber, testClient } from "./utils";
-import { afterAll } from "vitest";
+import { accounts, forkBlockNumber, localHttpUrl } from "./constants";
+import { anvilChain, setBlockNumber, testClient } from "./utils";
+import { createWalletClient, http } from "viem";
+import { privateKeyToAccount } from "viem/accounts";
+import { afterAll, beforeAll } from "vitest";
+import { erc20ABI } from "wagmi";
+
+beforeAll(async () => {
+  const client = createWalletClient({
+    account: privateKeyToAccount(accounts[0].privateKey),
+    chain: anvilChain,
+    pollingInterval: 1_000,
+    transport: http(localHttpUrl),
+  });
+  await client.deployContract({
+    abi: erc20ABI,
+  });
+});
 
 afterAll(async () => {
   // Reset the anvil instance to the same state it was in before the tests started.
