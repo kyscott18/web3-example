060e1e6d4c86943c54c7f1f6f70905d563cf7a1d
Kyle Scott <kyscott@umich.edu>
2023-05-27 11:36:02 -0400

    somewhat working

--- contracts/src/ERC20.sol
+++ contracts/src/ERC20.sol
@@ -1,3 +1,8 @@
-import { ERC20 as solmateERC20 } from "solmate/ERC20.sol";
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.0;
 
-contract ERC20 is solmateERC20 {}
+import {ERC20 as solmateERC20} from "../lib/solmate/src/tokens/ERC20.sol";
+
+contract MockERC20 is solmateERC20 {
+    constructor(string memory _name, string memory _symbol, uint8 _decimals) solmateERC20(_name, _symbol, _decimals) {}
+}
